6f753ca64c0a3f9ef0bce3b6fc58a3865d246b59
Sunny Gupta <sunnymax2002@gmail.com>
2020-04-21 06:00:20 +0100

    Refactoring

--- source/covid-19-analysis.py
+++ source/covid-19-analysis.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python
  
 import pandas as pd
-from os import path
-import tempfile
+#from os import path
 from datetime import datetime
 import matplotlib.pyplot as plt
 import helper_functions as hf
 
-# Worldwide daily COVID-19 numbers by country: https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data/resource/55e8f966-d5c8-438e-85bc-c7a5a26f4863
-url = 'https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide.xlsx'
-# Filename with today's date
-fpath = path.join(tempfile.gettempdir(), datetime.now().strftime("%Y%m%d") + "-covid-19.xlsx")
-hf.GetFileFromUrl(url, fpath)
-
-# Read XLSX file into Pandas DataFrame
-print("Reading local file to load DataFrame...")
-df = pd.read_excel(fpath)
-
-#print("\n\nCOVID-19 DataFrame Info:")
-#df.info()
-
-# Sort by total # of cases
-#print("\n\nTop Countries in terms of Total Cases:")
-#df_cases = df.groupby('countriesAndTerritories').cases.agg([sum])
-#df_cases.sort_values(by='sum', inplace=True, ascending=False)
-#print(df_cases)
-
-#print("\n\nCOVID-19 Total Cases and Deaths, by Country:")
-# Group the table by countries, and calculate total cases and total deaths
-
-#df_cases_and_deaths = df.groupby('countriesAndTerritories').agg({
-#    'cases':['sum'],
-#    'deaths':['sum']
-#})
+# Get COVID-19 daily data into a DataFrame
+df = hf.GetCovid19Data()
 
 print("Processing DataFrame...")
 
@@ -42,7 +17,7 @@ df_cases_and_deaths = df.groupby('countriesAndTerritories').agg(
 )
 df_cases_and_deaths.info()
 df_cases_and_deaths['percent_death'] = (df_cases_and_deaths['total_deaths'] / df_cases_and_deaths['total_cases']) * 100
-print(df_cases_and_deaths)
+#print(df_cases_and_deaths)
 
 # Calculate ww cases, deaths and % deaths
 total_ww_cases = df_cases_and_deaths['total_cases'].sum()
@@ -54,23 +29,19 @@ df_cases_and_deaths.sort_values(by='total_cases', inplace=True, ascending=False)
 
 num_toprows = 20
 df_top_cases_and_deaths = df_cases_and_deaths.loc[df_cases_and_deaths.iloc[0:num_toprows].index]
-df_top_cases_and_deaths.info()
+#df_top_cases_and_deaths.info()
 
-# Obtain population data
-url = "http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=excel"
-fpath = "D:\Learning\My Code\git_repos\ml-learn\datasets\API_SP.POP.TOTL_DS2_en_excel_v2_935990.xls"
-hf.GetFileFromUrl(url, fpath)
-
-pop_df = pd.read_excel(fpath, header=3, usecols=['Country Name', '2018'])
+# Get World Population Data
+pop_df = hf.GetWorldPopulationData()
 pop_df = pop_df[pop_df['Country Name'].isin(df_top_cases_and_deaths.index)]
-print(pop_df)
+#print(pop_df)
 
 pop_df = pop_df.set_index('Country Name')
-print(pop_df)
+#print(pop_df)
 
 #, fill_value=-1
 pop_df = pop_df.reindex(df_top_cases_and_deaths.index)
-print(pop_df)
+#print(pop_df)
 
 # Calculate cases as milli-% of country population
 df_top_cases_and_deaths['cases_per_pop'] = df_top_cases_and_deaths['total_cases'] / pop_df['2018'] * 1000
@@ -94,27 +65,13 @@ pdr.set_ylabel('Percentage Deaths')
 pcp.bar(df_top_cases_and_deaths.index, df_top_cases_and_deaths.cases_per_pop, color='green')
 pcp.set_ylabel('Cases as Milli-% Population')
 
-def autolabel(rects, ax):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
 # Add labels above bars in total cases chart
 #autolabel(rc1, pc)
 
 fig.suptitle('COVID-19 Analysis of Top 20 Countries by Total Cases: ' + datetime.now().strftime("%d %B, %Y"), fontsize=20)
 plt.xticks(rotation=90)
 
-#ax2 = ax.twinx()
-#df_top_cases_and_deaths.plot()
-#df_top_cases_and_deaths.plot(y='percent_death', kind='bar')
-
-#plt.show()
-
-gpath = path.join(tempfile.gettempdir(), datetime.now().strftime("%Y%m%d") + "-covid-19.png")
-plt.savefig(gpath)
+# TODO how to show as well as save to file?
+plt.show()
+gpath = hf.GetTempFileFullPath(datetime.now().strftime("%Y%m%d") + "-covid-19.png")
+#plt.savefig(gpath)
--- source/helper_functions.py
+++ source/helper_functions.py
@@ -1,9 +1,12 @@
 import requests
 from os import path
+import pandas as pd
+import tempfile
+from datetime import datetime
 
 def GetFileFromUrl(url, fpath):
     if(not path.exists(fpath)):
-        print("Downloading dataset to local file...")
+        print("[HF] Downloading dataset to local file: " + fpath)
         # Download from URL into a file-like object
         uf = requests.get(url)
 
@@ -11,3 +14,45 @@ def GetFileFromUrl(url, fpath):
         f = open(fpath, 'wb')
         f.write(uf.content)
         f.close()
+
+def GetTempFileFullPath(fname):
+    return path.join(tempfile.gettempdir(), fname)
+
+
+def GetCovid19Data():
+    # Worldwide daily COVID-19 numbers by country: https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data/resource/55e8f966-d5c8-438e-85bc-c7a5a26f4863
+    url = 'https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide.xlsx'
+    print("[HF] Obtaining Data from " + url)
+    # Filename with today's date
+    fpath = path.join(tempfile.gettempdir(), datetime.now().strftime("%Y%m%d") + "-covid-19.xlsx")
+    GetFileFromUrl(url, fpath)
+
+    # Read XLSX file into Pandas DataFrame
+    print("[HF] Reading local file to load DataFrame...")
+    return pd.read_excel(fpath)
+
+def GetWorldPopulationData():
+    # Obtain population data
+    url = "http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=excel"
+    print("[HF] Obtaining Data from " + url)
+    fpath = "D:\Learning\My Code\git_repos\ml-learn\datasets\API_SP.POP.TOTL_DS2_en_excel_v2_935990.xls"
+    GetFileFromUrl(url, fpath)
+
+    print("[HF] Reading local file to load DataFrame...")
+    # Ignore first 3 rows in XLS sheet0
+    # TODO find the latest year column instead of hard-coding to '2018'
+    print("[HF] Population Data from Year 2018...")
+    return pd.read_excel(fpath, header=3, usecols=['Country Name', '2018'])
+
+# Usage Example:
+#   rc1 = pc.bar(df_top_cases_and_deaths.index, df_top_cases_and_deaths.total_cases)
+#   AutolabelBarChart(rc1, pc)
+def AutolabelBarChart(rects, ax):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
